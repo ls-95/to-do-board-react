3c63042f711e47cf210c9aa19ecb6b0036ac738a
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-12-13 10:31:59 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Input from "./components/Input";
+
+function App() {
+  return (
+    <>
+      <h1>To Do Board</h1>
+      <Input />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+const Input = () => {
+  return (
+    <>
+      <h1>Add a task</h1>
+    </>
+  );
+};
+
+export default Input;
