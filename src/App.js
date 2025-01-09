412836a7f5aae3f5c3cdd21eba2ed2a7c68b4893
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-01-09 14:54:25 +0100

    "add task" functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Input from "./components/Input";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
   return (
-    <>
-      <h1>To Do Board</h1>
-      <Input />
-    </>
+    <div className="flex flex-col items-center justify-center py-8 gap-4">
+      <h1 className="text-xl font-semibold">To Do Board</h1>
+      <Input taskList={taskList} setTaskList={setTaskList} />
+      <div>
+        {taskList.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </div>
+    </div>
   );
 }
 
